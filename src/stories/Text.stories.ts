9cca8424c8c1d1cd4a609565ad66000d7690720d
Krishna Agarwal <krishna@gkmit.co>
2024-05-28 11:32:45 +0530

    chore: Update build script to remove diagnostics flag

---
+++ src/stories/Text.stories.ts
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from "@storybook/react/types-6-0";
+import Text from "../Text/index";
+
+const meta = {
+  title: "Example/Text",
+  component: Text,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} as Meta;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    text: "Hello, World!",
+  },
+} as Story;
