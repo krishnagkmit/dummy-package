2e453dd793b021dc70122568211b202c908970b7
Krishna Agarwal <krishna@gkmit.co>
2024-05-28 12:48:52 +0530

    chore: Update build script and add storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,14 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../dist/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack"
   ],
   framework: {
     name: "@storybook/react-vite",
